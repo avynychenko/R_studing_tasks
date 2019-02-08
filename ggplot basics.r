0e2c743d80d98dac9e9dbf54a3153dfcc94abdf8
Nastya <anastasija.vinichenko@gmail.com>
2019-02-08 12:34:04 +0200

    hello

--- ggplot basics.r
+++ ggplot basics.r
@@ -25,3 +25,6 @@ obj <- ggplot(data = mydata , aes(x = Hair, y = Freq, fill = Eye)) +
   geom_bar(stat="identity", position = position_dodge()) +                       
   scale_fill_manual(values=c("Brown", "Blue", "Darkgrey", "Darkgreen"))
 obj
+
+
+ПРИВЕТ!!!!!
