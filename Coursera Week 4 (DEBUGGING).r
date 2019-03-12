e4c20efff58882505d76362d5d95e7de6529a2df
Anastasiia Vynychenko <anastasija.vinichenko@gmail.com>
2019-03-12 17:43:50 +0200

    Completed tasks Coursera (tryCatch, stop)

--- Coursera Week 4 (DEBUGGING).r
+++ Coursera Week 4 (DEBUGGING).r
@@ -71,3 +71,49 @@ rankhospital <- function(state, outcome, num) {
     }
   } 
 }
+
+# Task 3
+
+# Write a function called rankall that takes two arguments: an outcome name (outcome) and a hospital ranking (num). The function 
+# reads the outcome-of-care-measures.csv le and returns a 2-column data frame containing the hospital in each state that has the 
+# ranking specied in num. The function should return a value for every state (some may be NA). The rst column in the data frame is 
+# named hospital, which contains the hospital name, and the second column is named state, which contains the 2-character abbreviation 
+# for the state name. Hospitals that do not have data on a particular outcome should be excluded from the set of hospitals when 
+# deciding the rankings.
+
+# The function should check the validity of its arguments. If an invalid outcome value is passed to rankall, the function should 
+# throw an error via the stop function with the exact message "invalid outcome". The num variable can take values "best", "worst", 
+# or an integer indicating the ranking (smaller numbers are better). If the number given by num is larger than the number of 
+# hospitals in that state, then the function should return NA.
+
+library(data.table)
+library(dplyr)
+
+rankall <- function(outcome, num = "best") {
+  data <- read.csv("https://raw.githubusercontent.com/avynychenko/R_studing_tasks/master/Data%20for%20tasks/Coursera%20Week%204%20debugging/outcome-of-care-measures.csv", 
+  colClasses = "character")
+  names(data)[c(11, 17, 23)] <- c("heart attack", "heart failure", "pneumonia")
+  data$State <- as.factor(data$State)
+  options(warn = -1)
+  res <- tryCatch( {as.data.table(data)[!is.na(as.numeric(get(outcome)))][order(as.numeric(get(outcome)), Hospital.Name)]}, error = function(x) "1")
+  if (res == "1") {
+    stop("invalid outcome")
+  } else {
+    sorted <- as.data.table(data)[!is.na(as.numeric(get(outcome)))][order(as.numeric(get(outcome)), Hospital.Name)]
+    if (num == "best") {
+      sorted %>% 
+        group_by(State) %>% 
+        slice(1) %>% 
+        select(hospital = Hospital.Name,
+                  state = State)
+    } else if (num == "worst") {
+      sorted %>% 
+        group_by(State) %>% 
+        summarize(hospital = tail(Hospital.Name, 1)) %>% 
+        select(hospital, state = State)
+    } else {
+    sorted %>%
+        group_by(State) %>% 
+        summarize(hospital = Hospital.Name[num]) %>% 
+        select(hospital, state = State)
+  } } }
